eb21cfc2653c488739f050398135d156d531cdd7
Obaidur Rahman <obaidurrahman@Obaidurs-MacBook-Air.local>
2021-07-08 00:22:58 +0600

    Alhamdulillah ...show up country name & details project is done

--- app.js
+++ app.js
@@ -12,10 +12,34 @@ const displayCountries = countries => {
         const countryInfo = `
                 <h3 class="country-name">${country.name}</h3>
                 <p>${country.capital}</p>
+                <button onclick="displayCountryDetails('${country.name}')">Show details</button>
         `
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
 
     });
+
+}
+const displayCountryDetails = name =>{
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    fetch(url)
+    .then(res =>res.json())
+    .then(data => renderCountryInfo(data[0]))
+    
+}
+const renderCountryInfo = country =>{
+    console.log(country);
+    const countryDiv = document.getElementById('countryDetails');
+    countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <p>Currencies Name: ${country.currencies[0].name}</p>
+        <p>Language: ${country.languages[0].name}</p>
+        <img src="${country.flag}">
+        
+    
+    `
+    
 
 }
