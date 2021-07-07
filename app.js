b13487fb6cd783db8729217596e4ba8f273b4cc1
Obaidur Rahman <obaidurrahman@Obaidurs-MacBook-Air.local>
2021-07-07 23:01:53 +0600

    added css

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ fetch('https://restcountries.eu/rest/v2/all')
 
 
 const displayCountries = countries =>{
-    const countriesDiv = document.getElementById('country');
+    const countriesDiv = document.getElementById('countries');
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
         const countryDiv = document.createElement('div');
+        countryDiv.className = 'country';
         const countryInfo = `
-                <h3>${country.name}</h3>
+                <h3 class="country-name">${country.name}</h3>
                 <p>${country.capital}</p>
         `
         countryDiv.innerHTML = countryInfo;
